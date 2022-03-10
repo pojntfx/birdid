5a9a6027d5c98754cd09f60613b728d2e2d6bd82
Felix Pojtinger <felix@pojtinger.com>
2022-03-11 00:35:28 +0100

    feat: Add basic replied, retweeted and mentioned search

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # birdid
+
 Bird + Cupid: Find the first interaction between two Twitter users
+
+## Overview
+
+🚧 This project is a work-in-progress! Instructions will be added as soon as it is usable. 🚧
+
+## License
+
+birdid (c) 2022 Felix Pojtinger and contributors
+
+SPDX-License-Identifier: AGPL-3.0
---
+++ main.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"golang.org/x/oauth2/clientcredentials"
+)
+
+type connection struct {
+	from  string
+	to    string
+	tweet twitter.Tweet
+}
+
+func main() {
+	clientID := flag.String("client-id", "", "Twitter API client ID (can also be set using the CLIENT_ID env variable)")
+	clientSecret := flag.String("client-secret", "", "Twitter API client secret (can also be set using the CLIENT_SECRET env variable)")
+
+	candidateOne := flag.String("candidate-one", "", "First candidate's Twitter handle")
+	candidateTwo := flag.String("candidate-two", "", "Second candidate's Twitter handle")
+
+	limit := flag.Int("limit", 1000, "Maximum amount of events to look back in timeline")
+
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+
+	flag.Parse()
+
+	if *clientID == "" {
+		*clientID = os.Getenv("CLIENT_ID")
+	}
+
+	if *clientSecret == "" {
+		*clientSecret = os.Getenv("CLIENT_SECRET")
+	}
+
+	if *candidateOne == "" {
+		panic("empty first candidate's Twitter handle")
+	}
+
+	if *candidateTwo == "" {
+		panic("empty second candidate's Twitter handle")
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	config := &clientcredentials.Config{
+		ClientID:     *clientID,
+		ClientSecret: *clientSecret,
+		TokenURL:     "https://api.twitter.com/oauth2/token",
+	}
+	httpClient := config.Client(ctx)
+
+	client := twitter.NewClient(httpClient)
+
+	connections := []connection{}
+
+	for candidateOne, candidateTwo := range map[string]string{*candidateOne: *candidateTwo, *candidateTwo: *candidateOne} {
+		maxID := int64(math.MaxInt64 - 1)
+		sinceID := int64(0)
+		curr := 0
+
+		var earliestCandidate twitter.Tweet
+
+		for maxID > sinceID {
+			if curr >= *limit {
+				break
+			}
+
+			results, _, err := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
+				ScreenName: candidateOne,
+				SinceID:    sinceID,
+				MaxID:      maxID,
+			})
+			if err != nil {
+				panic(err)
+			}
+
+			curr += len(results)
+
+			newMaxID := maxID
+			for _, tweet := range results {
+				if tweet.ID < newMaxID {
+					newMaxID = tweet.ID
+				}
+
+				if *verbose {
+					log.Println("Evaluating candidate for connection", candidateOne, "to", candidateTwo+":", tweet.ID, tweet.CreatedAt, tweet.Text)
+				}
+
+				if earliestCandidate.ID == 0 || earliestCandidate.ID > tweet.ID {
+					if tweet.InReplyToScreenName == candidateTwo {
+						earliestCandidate = tweet
+
+						continue
+					}
+
+					if tweet.RetweetedStatus != nil && tweet.RetweetedStatus.InReplyToScreenName == candidateTwo {
+						earliestCandidate = tweet
+
+						continue
+					}
+
+					if strings.Contains(tweet.Text, candidateTwo) {
+						earliestCandidate = tweet
+
+						continue
+					}
+				}
+			}
+
+			if newMaxID == maxID {
+				break
+			}
+
+			maxID = newMaxID
+		}
+
+		connections = append(connections, connection{
+			from:  candidateOne,
+			to:    candidateTwo,
+			tweet: earliestCandidate,
+		})
+	}
+
+	for _, connection := range connections {
+		fmt.Printf(
+			"Earliest contact from %v to %v: Tweet with ID %v at %v with URL %v and text %v\n",
+			connection.from,
+			connection.to,
+			connection.tweet.ID,
+			connection.tweet.CreatedAt,
+			"https://twitter.com/"+path.Join(connection.from, "status", connection.tweet.IDStr),
+			connection.tweet.Text,
+		)
+	}
+}
